2b325a9deca97c49116f897cf84e281411321598
Giancarlo Kramer <gianramiress@hotmail.com>
2023-04-04 22:35:12 -0300

    código aula 6 - sorteador composto

--- aula6/js/script.js
+++ aula6/js/script.js
@@ -48,7 +48,7 @@ function sortear(){
 function sortear2(){
     let min = parseInt(document.getElementById('min').value);
     let max = parseInt(document.getElementById('max').value);
-    let sortear = parseInt(Math.random() * (max - min) + min);
+    let sortear = parseInt((Math.random() * (max - min) + min));
     document.getElementById('sorteio2').innerHTML = `O valor sorteado é ${sortear}`;
 
 
